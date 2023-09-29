104ae4381bc61e91e889d5562066bf93078b66f4
Edrees Wahezi <sewahezi@gmail.com>
2023-09-28 23:36:36 -0700

    converted astro.config and rss.xml js files to typescript

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,8 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { type RSSFeedItem } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
     let blog = await getCollection('posts'); 
     //sort to put newest post first
     blog = blog.sort((a, b) => Date.parse(b.data.date.toDateString()) - Date.parse(a.data.date.toDateString()));
@@ -9,7 +10,7 @@ export async function GET(context) {
     return rss({
         title: 'Edrees Wahezi | Blog',
         description: 'I write stuff here',
-        site: context.site,
+        site: context.site!,
         items: blog.map((post) => ({
             title: post.data.title,
             pubDate: post.data.date,
